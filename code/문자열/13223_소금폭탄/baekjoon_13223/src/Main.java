429ac483ed00523a99b4b28c4f0bc867ae61af43
kimtg1997 <kimtg1997@pusan.ac.kr>
2024-04-30 01:27:12 +0900

    13223_소금폭탄

---
+++ code/문자열/13223_소금폭탄/baekjoon_13223/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.LocalDate;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String st1 = br.readLine();
+        SimpleDateFormat format = new SimpleDateFormat("hh:mm:ss");
+        LocalDate date1 = st1
+
+
+
+
+    }
+}
